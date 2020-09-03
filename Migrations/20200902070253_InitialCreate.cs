4af445729a1e81fed825efd34dcee53af731c800
SujoyBasak <sujoyr723@gmail.com>
2020-09-03 10:19:00 +0530

    Source Code Added

---
+++ Migrations/20200902070253_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XyzOfficeEmployeeTracker.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    empId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    deptId = table.Column<int>(nullable: false),
+                    deptName = table.Column<string>(nullable: true),
+                    salary = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.empId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalaryDetails",
+                columns: table => new
+                {
+                    salaryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    empId = table.Column<int>(nullable: false),
+                    date = table.Column<DateTime>(nullable: false),
+                    salary = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalaryDetails", x => x.salaryId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "SalaryDetails");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XyzOfficeEmployeeTracker.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int empId { get; set; }
+        public string Name { get; set; }
+        public int deptId { get; set; }
+        public string deptName { get; set; }
+        public int salary { get; set; }
+    }
+}
